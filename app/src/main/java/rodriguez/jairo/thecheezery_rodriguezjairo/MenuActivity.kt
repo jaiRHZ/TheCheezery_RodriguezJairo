309f836ef7e625bfe62a0e4b6b2b9035805baba5
jaiRHZ <jairo.rodriguez213248@potros.itson.edu.mx>
2025-07-10 11:33:12 -0700

    Funcional

--- app/src/main/java/rodriguez/jairo/thecheezery_rodriguezjairo/MenuActivity.kt
+++ app/src/main/java/rodriguez/jairo/thecheezery_rodriguezjairo/MenuActivity.kt
@@ -1,6 +1,8 @@
 package rodriguez.jairo.thecheezery_rodriguezjairo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,50 @@ import androidx.core.view.WindowInsetsCompat
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnCold: Button = findViewById(R.id.button_cold_drinks)
+        val btnHot: Button = findViewById(R.id.button_hot_drinks)
+        val btnSweets: Button = findViewById(R.id.button_sweets)
+        val btnSalties: Button = findViewById(R.id.button_salties)
+        val btnCombos: Button = findViewById(R.id.button_combos)
+        val btnCustom: Button = findViewById(R.id.button_customizable)
+
+
+        btnCold.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "coldDrinks")
+            startActivity(intent)
+        }
+
+        btnHot.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "hotDrinks")
+            startActivity(intent)
+        }
+
+        btnSweets.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "sweets")
+            startActivity(intent)
+        }
+
+        btnSalties.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "salties")
+            startActivity(intent)
+        }
+
+        btnCombos.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "combos")
+            startActivity(intent)
+        }
+
+        btnCustom.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "customizable")
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/rodriguez/jairo/thecheezery_rodriguezjairo/ProductosActivity.kt
@@ -0,0 +1,135 @@
+package rodriguez.jairo.thecheezery_rodriguezjairo
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProductosActivity : AppCompatActivity() {
+    var coldDrinks = ArrayList<Product>()
+    var hotDrinks = ArrayList<Product>()
+    var sweets = ArrayList<Product>()
+    var salties = ArrayList<Product>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_productos)
+
+        agregarProductos()
+
+        var image: ImageView = findViewById(R.id.titleDrinks)
+
+        val listview: ListView = findViewById(R.id.listview)
+        /*var adaptador: AdaptadorProductos = AdaptadorProductos(this, coldDrinks)
+        listview.adapter = adaptador*/
+
+        //Obtener el tipo de menú desde el intent
+        val menuType = intent.getStringExtra("menuType")
+
+        //Elige la lista de productos
+        val productosMostrar = when (menuType) {
+            "coldDrinks" -> coldDrinks
+            "hotDrinks" -> hotDrinks
+            "sweets" -> sweets
+            "salties" -> salties
+            else -> coldDrinks
+        }
+
+        when (productosMostrar) {
+            hotDrinks -> image.setImageResource(R.drawable.hotdrinks)
+            sweets -> image.setImageResource(R.drawable.sweets)
+            salties -> image.setImageResource(R.drawable.salties)
+            else -> image.setImageResource(R.drawable.colddrinks)
+        }
+
+        //Asignar el adaptador con la lista adecuada
+        val adaptador = AdaptadorProductos(this, productosMostrar)
+        listview.adapter = adaptador
+    }
+
+    fun agregarProductos(){
+        //Cold drinks
+        coldDrinks.add(Product("Caramel Frap", R.drawable.caramelfrap, "Caramel syrup meets coffee, milk and ice and whipped cream and buttery caramel sauce layer the love on top.", 5.0))
+        coldDrinks.add(Product("Chocolate Frap", R.drawable.chocolatefrap, "Rich mocha-flavored sauce meets up with chocolaty chips, milk and ice for a blender bash.", 6.0))
+        coldDrinks.add(Product("Cold Brew", R.drawable.coldbrew, "Created by steeping medium-to-coarse ground coffee in room temperature water for 12 hours or longer.", 3.0))
+        coldDrinks.add(Product("Matcha Latte", R.drawable.matcha, "Leafy taste of matcha green tea powder with creamy milk and a little sugar for a flavor balance that will leave you feeling ready and raring to go.", 4.0))
+        coldDrinks.add(Product("Oreo Milkshake", R.drawable.oreomilkshake, "Chocolate ice cream, and oreo cookies. Topped with whipped cream with cocoa and chocolate syrup.", 7.0))
+        coldDrinks.add(Product("Peanut Milkshake", R.drawable.peanutmilkshake, "Vanilla ice cream, mixed with peanut butter and chocolate.", 7.0))
+
+        //Hot Drinks
+        hotDrinks.add(Product("Latte", R.drawable.latte, "Coffee drink with espresso ans steamed milk", 6.0))
+        hotDrinks.add(Product("Hot chocolate", R.drawable.hotchocolate, "Heated drink consisting of shaved chocolate, topped with marshmallows.", 5.0))
+        hotDrinks.add(Product("Espresso", R.drawable.espresso, "Full-flavored, concentrated form of coffee", 4.0))
+        hotDrinks.add(Product("Chai Latte", R.drawable.chailatte, "Spiced tea concentrate with milk", 6.0))
+        hotDrinks.add(Product("Capuccino", R.drawable.capuccino, "A cappuccino is an espresso-based coffee drink, prepared with steamed foam.", 7.0))
+        hotDrinks.add(Product("American coffee", R.drawable.americano, "Espresso with hot water", 2.0))
+
+        //Seets
+        sweets.add(Product("Blueberry cake", R.drawable.blueberrycake, "Vanilla cake flavor, topped with cheese topping and blueberries.", 6.0))
+        sweets.add(Product("Chocolate cupcake", R.drawable.chocolatecupcake, "Chocolate cupcakes topped with butter cream and cherries", 3.0))
+        sweets.add(Product("Lemon tartalette", R.drawable.lemontartalette, "Pastry shell with a lemon flavored filling", 4.0))
+        sweets.add(Product("Red Velvet cake", R.drawable.redvelvetcake, "Soft, moist, buttery cake topped with an easy cream cheese frosting.", 6.0))
+        sweets.add(Product("Cherry cheesecake", R.drawable.strawberrycheesecake, "This cherry topped cheesecake is positively creamy and delicious and will be your new favorite dessert.", 7.0))
+        sweets.add(Product("Tiramisu", R.drawable.tiramisu, "Coffee-flavored Italian dessert", 6.0))
+
+        //Salties
+        salties.add(Product("Chicken crepes", R.drawable.chickencrepes, "Fine crepes stuffed with Alfredo chicken, spinach and mushrooms.", 6.0))
+        salties.add(Product("Club Sandwich", R.drawable.clubsandwich, "A delicious sandwich served with french fries.", 5.0))
+        salties.add(Product("Panini", R.drawable.hampanini, "Sandwich made with Italian bread  served warmed by grilling.", 4.0))
+        salties.add(Product("Philly cheese steak", R.drawable. phillycheesesteak, "Smothered in grilled onions, green peppers, mushrooms, and Provolone.", 6.0))
+        salties.add(Product("Nachos", R.drawable. nachos, "Tortilla chips layered with beef and   melted cheddar cheese. Served with fried beans, guacamole, pico de gallo, and sour topping.",  7.0))
+
+
+    }
+
+    private class AdaptadorProductos:BaseAdapter{
+        var productos = ArrayList<Product>()
+        var contexto: Context? = null
+
+        constructor(contexto: Context, productos: ArrayList<Product>){
+            this.productos = productos
+            this.contexto = contexto
+        }
+
+        //Cuantos elementos de la lista
+        override fun getCount(): Int {
+            return productos.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return productos[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var producto = productos[position]
+            var inflador = LayoutInflater.from(contexto)
+            var vista = inflador.inflate(R.layout.producto_view, null)
+
+            var imagen = vista.findViewById(R.id.producto_img) as ImageView
+            var nombre = vista.findViewById(R.id.producto_nombre) as TextView
+            var desc = vista.findViewById(R.id.producto_desc) as TextView
+            var precio = vista.findViewById(R.id.producto_precio) as TextView
+
+            imagen.setImageResource(producto.image)
+            nombre.setText(producto.name)
+            desc.setText(producto.description)
+            precio.setText("$${producto.price}")
+
+            return vista
+        }
+    }
+
+}
